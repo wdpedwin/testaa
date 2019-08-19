32f55e1294be9f6455225b86327c7d0ea53d56f1
wdpedwin <wdpedwin@163.com>
2019-08-19 12:27:30 +0800

    create repository

---
+++ mainapp/__init__.py
@@ -0,0 +1,4 @@
+from flask import Flask
+import settings
+app = Flask(__name__)
+app.config.from_object(settings.Dev)
---
+++ settings.py
@@ -0,0 +1,9 @@
+
+class Dev():
+    ENV = 'development'
+    DEBUG = True
+
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@10.36.174.19/edu?charset=utf8'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_ECHO = True
